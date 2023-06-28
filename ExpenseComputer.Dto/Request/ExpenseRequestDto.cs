46f5fcf2d8d60c213a5a4089e77ffc92b869ef0e
Nyrico Angelo M. Cuartel <angelocuartel@gmail.com>
2023-06-28 13:53:46 +0800

    added expense request dto

---
+++ ExpenseComputer.Dto/Request/ExpenseRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseComputer.Dto.Request
+{
+    public class ExpenseRequestDto
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
